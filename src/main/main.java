e19b52908ae6ff9f08cb7495adf6a607f303e2dc
TheDutchMC04 <tobias64a@gmail.com>
2019-01-11 22:41:49 +0100

    switch from barcode to QR

--- src/main/main.java
+++ src/main/main.java
@@ -2,19 +2,29 @@ package main;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 
-import net.sourceforge.barbecue.Barcode;
-import net.sourceforge.barbecue.BarcodeException;
-import net.sourceforge.barbecue.BarcodeFactory;
-import net.sourceforge.barbecue.BarcodeImageHandler;
-import net.sourceforge.barbecue.output.OutputException;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+//import net.sourceforge.barbecue.Barcode;
+//import net.sourceforge.barbecue.BarcodeException;
+//import net.sourceforge.barbecue.BarcodeFactory;
+//import net.sourceforge.barbecue.BarcodeImageHandler;
+//import net.sourceforge.barbecue.output.OutputException;
 
 public class main {
 
 	static String barcodeValue;
-	static String startAt, amount;
+	static String startAt;
 	static int atCurrentlyCount, atCurrently;
+	
+	static int height, width, amount;
 	
 	public static void main(String[] args) {
 		
@@ -25,20 +35,20 @@ public class main {
 		
 		barcodeValue = mainGui.inputLinkValue;
 		startAt = mainGui.startNumberValue;
-		amount = mainGui.amountNumberValue;
 		
-		int amountInt = Integer.parseInt(amount);
+		height = Integer.parseInt(mainGui.height);
+		width = Integer.parseInt(mainGui.width);
+		
+		amount = Integer.parseInt(mainGui.amountNumberValue);
 		atCurrently = atCurrentlyCount + Integer.parseInt(startAt);
 		
-		while(amountInt >= atCurrentlyCount) {
+		while(amount >= atCurrentlyCount) {
 			if(mainGui.isEntered) {
 				try {
-					outputtingBarcode128x2();
-				} catch (BarcodeException e) {
+					createQR(barcodeValue, width, height);
+				} catch (WriterException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (OutputException e) {
 					e.printStackTrace();
 				}
 			} else {
@@ -53,9 +63,11 @@ public class main {
 		}
 	}
 	
-	public static void outputtingBarcode128x2() throws BarcodeException, IOException, OutputException {
+	/*public static void outputtingBarcode() throws BarcodeException, IOException, OutputException {
 		Barcode barcode3;
-		barcode3 = BarcodeFactory.createCode128(barcodeValue + "?=barcodeID:" + atCurrently);
+		barcode3 = BarcodeFactory.createPDF417(barcodeValue + "?=barcodeID:" + atCurrently);
+	    barcode3.setBarHeight(40);
+	    barcode3.setBarWidth(2);
 		barcode3.setResolution(500);
 		barcode3.setDrawingText(false);
 		BarcodeImageHandler.savePNG(barcode3, new File(Integer.toString(atCurrently) + ".png"));
@@ -64,8 +76,27 @@ public class main {
 		
 		atCurrently = atCurrently + 1;
 		atCurrentlyCount = atCurrentlyCount + 1;
-		
 		System.out.println("Currently at: " + atCurrently);
-}
+	}*/
 	 
+	public static void createQR(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text + "?=senderID:" +atCurrently, BarcodeFormat.QR_CODE, width, height);
+		
+		try {
+		    String basePath = new File("").getAbsolutePath();
+		    System.out.println(basePath);
+		    File file = new File(String.format(atCurrently + ".png"));
+		    MatrixToImageWriter.writeToFile(bitMatrix, "PNG", file);
+
+		    System.out.println("Barcode " + atCurrently + " has been generated");
+
+		    atCurrently = atCurrently + 1;
+		    atCurrentlyCount = atCurrentlyCount + 1;
+		} catch (Exception e) {
+		    atCurrently = 0;
+		}
+		System.out.println("Currently at: " + atCurrently);
+		
+	}
 }
--- src/main/mainGui.java
+++ src/main/mainGui.java
@@ -12,7 +12,7 @@ import javax.swing.JTextField;
 @SuppressWarnings("serial")
 public class mainGui extends JFrame {
 
-	public static String startNumberValue, amountNumberValue, inputLinkValue;
+	public static String startNumberValue, amountNumberValue, inputLinkValue, path, height, width;
 	public static Boolean isEntered = false;
 		
 	public static void init() {
@@ -32,6 +32,11 @@ public class mainGui extends JFrame {
 		inputStartNumberText.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 25));
 		inputStartNumberText.setBounds(1, 40, 100, 30);
 		frame.getContentPane().add(inputStartNumberText);
+		
+		JLabel startAtNote = new JLabel("Start at 0 if first one!");
+		startAtNote.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 25));
+		startAtNote.setBounds(170, 40, 300, 30);
+		frame.getContentPane().add(startAtNote);
 
 		JLabel inputAmountText = new JLabel("Amount:");
 		inputAmountText.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 25));
@@ -43,6 +48,15 @@ public class mainGui extends JFrame {
 		inputLinkText.setBounds(1, 120, 100, 30);
 		frame.getContentPane().add(inputLinkText);
 		
+		JLabel inputHeightText = new JLabel("Height:");
+		inputHeightText.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 25));
+		inputHeightText.setBounds(1, 160, 100, 35);
+		frame.getContentPane().add(inputHeightText);
+
+		JLabel inputWidthText = new JLabel("Width:");
+		inputWidthText.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 25));
+		inputWidthText.setBounds(170, 160, 100, 30);
+		frame.getContentPane().add(inputWidthText);
 		
 		//Text Fields
 		JTextField inputStartNumber = new JTextField("");
@@ -56,10 +70,18 @@ public class mainGui extends JFrame {
 		JTextField inputLink = new JTextField("");
 		inputLink.setBounds(100, 124, 300, 30);
 		frame.getContentPane().add(inputLink);
+		
+		JTextField inputHeight = new JTextField("250");
+		inputHeight.setBounds(100, 160, 50, 30);
+		frame.getContentPane().add(inputHeight);
+		
+		JTextField inputWidth = new JTextField("250");
+		inputWidth.setBounds(250, 160, 50, 30);
+		frame.getContentPane().add(inputWidth);
 
 		//Button
 		JButton confirmButton = new JButton("Confirm");
-		confirmButton.setBounds(1, 160, 100, 30);
+		confirmButton.setBounds(1, 200, 100, 30);
 		frame.getContentPane().add(confirmButton);
 		
 		frame.setVisible(true);
@@ -69,9 +91,12 @@ public class mainGui extends JFrame {
 				startNumberValue = inputStartNumber.getText();
 				amountNumberValue = inputAmount.getText();
 				inputLinkValue = inputLink.getText();
+				height = inputHeight.getText();
+				width = inputWidth.getText();
+				
 				isEntered = true;
 				
-				System.out.print(startNumberValue + " " + amountNumberValue + " " + inputLinkValue);
+				System.out.println(path);
 				main.checkInput();
 			}
 		});
